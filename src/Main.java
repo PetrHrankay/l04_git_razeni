78668981683f097ab23439c9a51d6c1afae2a496
P3ANTMC <pean.tmc@seznam.cz>
2024-04-16 21:31:55 +0200

    Ukazka razeni seznamu
    Zakladni razeni s vyuzitim
    'Comparator.comparing(.....).'
    
    Ukazka pouziti '.reversed()''

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import cz.engeto.ja.Computer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +18,16 @@ public class Main {
         for (Computer computer : computers) {
             System.out.println(computer.getDescription() + " " + computer.getYearOfProduction() + " " + computer.getPrice());
         }
+
+        computers.sort(Comparator.comparing(Computer::getYearOfProduction));
+
+        System.out.println("\nSorted by year of production\n");
+        computers.forEach(System.out::println);
+
+        Collections.sort(computers, Comparator.comparing(Computer::getPrice).reversed());
+
+        System.out.println("\nSorted by price reversed\n");
+        computers.forEach(System.out::println);
 
     }
 }
